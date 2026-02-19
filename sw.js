c63b3022c35b1457351c238d6eb6b544aaf1d1c5
philippe fortune <philvercors@gmail.com>
2026-02-19 19:01:26 +0100

    premier commit

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pdc-app-v2';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './icon.png', // On met l'icône en cache
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Supprime les anciens caches lors d'une mise à jour
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
